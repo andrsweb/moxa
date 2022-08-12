78307ba9f6cd6ff1aea5f57be7e87f26ece7e1c4
proflinervk <proflinervk@gmail.com>
2022-08-12 03:04:59 +0300

    added portfolio section

--- src/js/sections/form/form.js
+++ src/js/sections/form/form.js
@@ -3,29 +3,35 @@ import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
 document.addEventListener( 'DOMContentLoaded', () => {
 
     'use strict'
+    
+    toggleForm()
 })
 
 // Form
-const headerForm    = document.querySelector( '.header-form' )
-const buttons       = document.querySelectorAll( '.nav-button' )
-const closeButton   = document.querySelector( '.close-button' )
-const targetElement = document.querySelector( '#form-lock' )
 
-buttons.forEach( button => {
+const toggleForm = () => {
 
-    button.addEventListener( 'click', () => {
-        if ( ! headerForm.classList.contains( 'opened' ) ) {
-            headerForm.classList.add( 'opened' )
-            disableBodyScroll( targetElement )
+    const headerForm    = document.querySelector( '.header-form' )
+    const buttons       = document.querySelectorAll( '.nav-button' )
+    const closeButton   = document.querySelector( '.close-button' )
+    const targetElement = document.querySelector( '#form-lock' )
+    
+    buttons.forEach( button => {
+    
+        button.addEventListener( 'click', () => {
+            if ( ! headerForm.classList.contains( 'opened' ) ) {
+                headerForm.classList.add( 'opened' )
+                disableBodyScroll( targetElement )
+            }
+        } )
+    } )
+    
+    // Close form
+    closeButton.addEventListener( 'click', () => {
+    
+        if ( headerForm.classList.contains( 'opened' ) ) {
+            headerForm.classList.remove( 'opened' )
+            enableBodyScroll( targetElement )
         }
     } )
-} )
-
-// Close form
-closeButton.addEventListener( 'click', () => {
-
-    if ( headerForm.classList.contains( 'opened' ) ) {
-        headerForm.classList.remove( 'opened' )
-        enableBodyScroll( targetElement )
-    }
-} )
+}
